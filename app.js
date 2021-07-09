49e770a26905901e11d537a68ff080d4318ebea2
vadimshift <vadimshift@yandex.ru>
2021-07-09 10:33:12 +0500

    реализовал логирование ошибок

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-// Слушаем 3000 порт
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
+app.use(requestLogger); // подключение логгера запросов
+app.use(errorLogger); // подключение логгера ошибок
