b65316c5b8f3305e38695313fe9a94903ad80257
vadimshift <vadimshift@yandex.ru>
2021-07-09 12:18:31 +0500

    Создал БД, настроил взаимодействие с ней

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
+
 
 app.use(requestLogger); // подключение логгера запросов
 app.use(errorLogger); // подключение логгера ошибок
+
+app.listen(PORT);
