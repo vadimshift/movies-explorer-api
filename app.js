8c8eee2bc4f3f23be57875d1b8458e797dc223fa
vadimshift <vadimshift@yandex.ru>
2021-07-15 19:16:14 +0500

    исправил ошибку лимитера

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const limiter = require('./middlewares/limiter');
+const { limiter } = require('./middlewares/limiter');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const processingErrors = require('./middlewares/processingErrors');
@@ -17,7 +17,7 @@ const app = express();
 app.use(requestLogger); // подключение логгера запросов
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect(process.env.DB_CONN, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
