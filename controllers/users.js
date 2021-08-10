9df904450664af3b514fae1437844fb544a18292
vadimshift <vadimshift@yandex.ru>
2021-08-10 15:47:35 +0500

    внес небольшие правки в функцию логина

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 const {
@@ -9,14 +9,14 @@ const {
   ValidationError,
   ConflictError,
   UnauthorizedError,
-} = require('../middlewares/errors');
+} = require("../middlewares/errors");
 
 function getUser(req, res, next) {
   User.findById(req.user._id)
     .then((data) => res.send(data))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new NotFoundError('Пользователь по указанному id не найден'));
+      if (err.name === "CastError") {
+        next(new NotFoundError("Пользователь по указанному id не найден"));
       } else {
         next(err);
       }
@@ -27,15 +27,23 @@ function getUser(req, res, next) {
 function updateUser(req, res, next) {
   const { name, email } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, email }, {
-    new: true,
-    runValidators: true,
-    upsert: false,
-  })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, email },
+    {
+      new: true,
+      runValidators: true,
+      upsert: false,
+    }
+  )
     .then((data) => res.send(data))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new BadRequestError('Переданы некорректные данные при обновлении профиля пользователя'));
+      if (err.name === "ValidationError") {
+        next(
+          new BadRequestError(
+            "Переданы некорректные данные при обновлении профиля пользователя"
+          )
+        );
       } else {
         next(err);
       }
@@ -44,24 +52,34 @@ function updateUser(req, res, next) {
 }
 
 function createUser(req, res, next) {
-  const {
-    name, email,
-  } = req.body;
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => User.create({
-      name, email, password: hash,
-    }))
-    .then((user) => res.status(201).send(
-      {
-        name: user.name, _id: user._id, email: user.email,
-      },
-    ))
+  const { name, email } = req.body;
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        email,
+        password: hash,
+      })
+    )
+    .then((user) =>
+      res.status(201).send({
+        name: user.name,
+        _id: user._id,
+        email: user.email,
+      })
+    )
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new ValidationError('Переданы некорректные данные при создании пользователя'));
-      } else if
-      (err.name === 'MongoError') {
-        next(new ConflictError('Пользователь с таким email уже зарегистрирован'));
+      if (err.name === "ValidationError") {
+        next(
+          new ValidationError(
+            "Переданы некорректные данные при создании пользователя"
+          )
+        );
+      } else if (err.name === "MongoError") {
+        next(
+          new ConflictError("Пользователь с таким email уже зарегистрирован")
+        );
       } else {
         next(err);
       }
@@ -71,36 +89,50 @@ function createUser(req, res, next) {
 
 function login(req, res, next) {
   const { email, password } = req.body;
-  User.findOne({ email }).select('+password')
+  User.findOne({ email })
+    .select("+password")
     .then((user) => {
       if (!user) {
-        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль, невозможно авторизоватся'));
+        return Promise.reject(
+          new UnauthorizedError(
+            "Неправильные почта или пароль, невозможно авторизоватся"
+          )
+        );
       }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            // хеши не совпали — отклоняем промис
-            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль, невозможно авторизоватся'));
-          }
-          // аутентификация успешна
-          // создаем токен
-          const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-          // возвращаем токен в куки, срок жизни 7 дней
-          res
-            .cookie('jwt', token, {
-              maxAge: 3600000 * 24 * 7,
-              httpOnly: true,
-            })
-            .send({ message: 'Успешный логин' })
-            .end();
-        });
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          // хеши не совпали — отклоняем промис
+          return Promise.reject(
+            new UnauthorizedError(
+              "Неправильные почта или пароль, невозможно авторизоватся"
+            )
+          );
+        }
+        // аутентификация успешна
+        // создаем токен
+        const token = jwt.sign(
+          { _id: user._id },
+          NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+          { expiresIn: "7d" }
+        );
+        // возвращаем токен в куки, срок жизни 7 дней
+        res
+          .cookie("jwt", token, {
+            maxAge: 3600000 * 24 * 7,
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+          })
+          .send({ email: user.email })
+          .end();
+      });
     })
     .catch(next);
 }
 
 function logout(req, res) {
-  res.clearCookie('jwt');
-  return res.send({ message: 'Выполнен выход' });
+  res.clearCookie("jwt");
+  return res.send({ message: "Выполнен выход" });
 }
 
 module.exports = {
