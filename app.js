c0be5f187c158fe1be4ab84892c89d71338efdf5
vadimshift <vadimshift@yandex.ru>
2021-08-10 22:47:44 +0500

    cors настраиваю

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
-const helmet = require('helmet');
-const { limiter } = require('./middlewares/limiter');
-const router = require('./routes/index');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const processingErrors = require('./middlewares/processingErrors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { errors } = require("celebrate");
+const helmet = require("helmet");
+const { limiter } = require("./middlewares/limiter");
+const router = require("./routes/index");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const processingErrors = require("./middlewares/processingErrors");
 
 const { NODE_ENV, DB_CONN, PORT } = process.env;
 
 const app = express();
-
+app.use(cors());
 app.use(requestLogger); // подключение логгера запросов
 
 // подключаемся к серверу mongo
-mongoose.connect(NODE_ENV === 'production' ? DB_CONN : 'mongodb://localhost:27017/bitfilmsdb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  NODE_ENV === "production" ? DB_CONN : "mongodb://localhost:27017/bitfilmsdb",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
 /* const whitelist = [
   'http://api.vadim.movies-explorer.nomoredomains.rocks',
@@ -39,8 +41,7 @@ mongoose.connect(NODE_ENV === 'production' ? DB_CONN : 'mongodb://localhost:2701
 }; */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(cors());
+
 app.use(limiter);
 app.use(helmet());
 
@@ -49,4 +50,4 @@ app.use(router); // подключение маршрутов
 app.use(errorLogger); // подключение логгера ошибок
 app.use(errors()); // ошибки celebrate
 app.use(processingErrors); // обработка ошибок
-app.listen(NODE_ENV === 'production' ? PORT : '3000');
+app.listen(NODE_ENV === "production" ? PORT : "3000");
